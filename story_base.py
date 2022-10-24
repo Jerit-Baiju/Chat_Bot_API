ad0dcbd26cae96c7a9e7e4e40da6c9ca7e3cc27d
Jerit Baiju <jeritalumkal@gmail.com>
2022-10-24 12:39:59 +0530

    .

--- story_base.py
+++ story_base.py
@@ -1,12 +1,11 @@
 from pprint import pprint
-from commands import run
+import commands
 
 
-lines = open('story.txt', 'r').read().replace('\n','').split('.')
-# print(lines)
-cmds = run(bot='elio', name='jerit')
-op = {"main": "ijk", "op": ["mkl"]}
-cmds.append(op)
-# print(cmds)
-f = open('trial.py', 'w')
-f.write(f'def run(bot, name):\n\tmy_commands = {cmds}\n\treturn my_commands')
+
+
+def run():
+    lines = open('story.txt', 'r').read().replace('\n','').split('.')
+    # print(lines)
+
+run()
