4085d02e96c7c93f1afed4bc85daa9048219a77c
Jerit Baiju <jeritalumkal@gmail.com>
2024-08-14 16:54:12 +0530

    Fix: API endpoint consistency and variable replacements
    This commit refactors the API endpoint to enhance consistency and addresses an issue with variable replacement in user responses. The updated endpoint ensures smoother integration and improves the accuracy of user-specific information in bot replies.

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
+import random
+
 from flask import Flask, jsonify, redirect
 from flask_cors import CORS
+
 import main
-import random
 import root
 
-app = Flask('__name__')
+app = Flask("__name__")
 CORS(app)
 
-debug=True
+debug = True
 if debug:
     import engine
+
     engine.run()
 
-@app.route('/')
-def index():
-    return redirect('https://jerit.herokuapp.com')
 
-@app.route('/api/<string:bot>/<string:user>/<string:intents>', methods=['GET'])
+@app.route("/")
+def index():
+    return redirect("https://jerit.herokuapp.com")
+
+
+@app.route("/api/<string:bot>/<string:user>/<string:intents>", methods=["GET"])
 def bot(bot, user, intents):
-    intents = str(intents).replace(bot,root.bot)
+    intents = str(intents).replace(bot, root.bot)
     op = main.run(intents)
     if type(op) == list:
         op = random.choice(op)
     op = op.replace(root.bot, bot)
     op = op.replace(root.name, user)
     response = jsonify(op.upper())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
     return response
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=debug)
+if __name__ == "__main__":
+    app.run(host="172.20.7.197", port=8000, debug=debug)
